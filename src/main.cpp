cd6259fe72b1ff655d3e2b5487a1882e405b2862
toolateralus <joshnjoshgaming@gmail.com>
2024-08-17 11:34:21 -0400

    chore: Refactor scene.yaml to update camera sky color and light intensity

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include "../include/node.hpp"
 #include "../include/engine.hpp"
 #include "../include/input.hpp"
-#include "../include/demo.hpp"
 #include "../include/fileio.hpp"
 
 #include <fstream>
@@ -18,8 +17,6 @@
 
 using namespace physics;
 
-
-
 // We do this to initialize the singletons and provide easy access here.
 static Engine &m_engine = Engine::current();
 static Input &m_input = Input::current();
@@ -29,22 +26,12 @@ shared_ptr<Material> &m_material = m_engine.m_material;
 Renderer &m_renderer =  m_engine.m_renderer;
 
 void setup_default_scene() {
- 
-  
   // SETUP FLOOR
   {
-    auto floor =
-        Node::instantiate(vec3(0, -10, 0),vec3(1, 1, 1));
+    auto floor = Node::instantiate(vec3(0, -10, 0),vec3(1, 1, 1));
     auto floor_mesh = floor->add_component<MeshRenderer>(m_material, Engine::RESOURCE_DIR_PATH + "/prim_mesh/rooms.obj");
     floor_mesh->color = vec4(0.5, 0.5, 0.5, 1.0f);
   }
-  
-  // SETUP PLAYER
-  {
-    auto player_node = Node::instantiate();
-    player_node->add_component<Car>();
-  }
-  
 }
 
 int main(int argc, char **argv) {
@@ -59,9 +46,6 @@ int main(int argc, char **argv) {
       setup_default_scene();
     }
     m_renderer.run();
-    YAML::Emitter out;
-    m_scene.serialize(out);
-    write_file(scene_path, out.c_str());
   } else {
     cout << "No scene file found, creating default scene\n";
     setup_default_scene();
